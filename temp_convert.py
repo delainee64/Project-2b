c46b70fe6ba09977420271107d62290ca8229171
Delainee Lenss <lenssd@oregonstate.edu>
2022-10-03 20:29:54 -0500

    Commit Message

---
+++ temp_convert.py
@@ -0,0 +1,12 @@
+# Author: Delainee Lenss
+# GitHub username: delainee64
+# Date: 10/02/2022
+# Description: Asks the user for a temperature in Celsius and then prints
+# the corresponding Fahrenheit temperature.
+
+print("Please enter a Celsius temperature.")
+c_temp = float(input())
+result = ((9 / 5) * c_temp)
+result += 32
+print("Please enter a Celsius temperature.")
+print(result)
